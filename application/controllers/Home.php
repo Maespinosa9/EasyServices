1f6179f6c3a287056006dd4420399d2ed7f7f994
Michael Espinosa <maespinosa9@misena.edu.co>
2016-08-21 15:07:22 -0500

    Inicio Forms

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,7 +9,7 @@ class Home extends CI_Controller
 	public function index()
 	{
 		$this->load->view('templates/Header');
-		$this->load->view('pages/Home');
+		$this->load->view('pages/Cliente');
 		$this->load->view('templates/Footer');
 	}
 }
---
+++ application/views/pages/Cliente.php
@@ -0,0 +1,3 @@
+<div class="panel panel-primary">
+<div class="panel-heading"><h1 class="panel-title"><strong>CLIENTES</strong></h1></div>
+</div>
