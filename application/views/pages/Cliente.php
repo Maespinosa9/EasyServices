85a1dca205ec383ac0df386ccd3aab0d13b8867d
J. Andres Boyaca <J. Andres Boyaca@LAPTOP-F78PISG9>
2016-09-03 21:31:12 -0500

    Ajuste forma

--- application/views/pages/Cliente.php
+++ application/views/pages/Cliente.php
@@ -1,57 +1,55 @@
 <div id="EasyContainer" name="EasyContainer">
-	<h3><strong>CLIENTES</strong></h3>
-	
-	  	<div id="toolbar" class="btn-group">
-	    	<button type="button" class="btn btn-default">
-	        	<img src="<?php echo base_url(); ?>\assets\image\add.jpg" class="img-rounded img-header"/>
-	    	</button>
-	    	<button type="button" class="btn btn-default">
-	        	<img src="<?php echo base_url(); ?>\assets\image\edit.jpg" class="img-rounded img-header"/>
-	    	</button>
-	    	<button type="button" class="btn btn-default">
-	        	<img src="<?php echo base_url(); ?>\assets\image\delete.jpg" class="img-rounded img-header"/>
-	   		</button>
-	 <table class="table table-striped table-bordered"  data-search="true">
-	 	<thead>
-	 		<th>Documento</th>
-	 		<th>Nombre</th>
-	 		<th>Ciudad</th>
-	 		<th>Direcci&oacute;n</th>
-	 		<th>Telefono</th>
-	 		<th>Celular</th>
-	 		<th>Correo</th>
-	 		<th>Requerimiento</th>
-	 		<th>Harck Observaciones</th>
-	 		<th>Observaciones</th>
-	 	</thead>
-	 	<tbody>
-	 		<tr>
-	 			<td>123456</td>
-	 			<td>Michael Alexander Espinosa Rico</td>
-	 			<td>Bogotá</td>
-	 			<td>Calle 118 # 20 - 53</td>
- 				<td>8621208</td>
- 				<td>3214325244</td>
- 				<td>maes9104@gmail.com</td>
- 				<td>Empleada por días</td>
- 				<td>No se que pueda ser esto</td>
- 				<td>Observaciones para cliente de prueba</td>
-	 		</tr>	
- 			<tr>
-	 			<td>123456</td>
-	 			<td>Michael Alexander Espinosa Rico</td>
-	 			<td>Bogotá</td>
-	 			<td>Calle 118 # 20 - 53</td>
- 				<td>8621208</td>
- 				<td>3214325244</td>
- 				<td>maes9104@gmail.com</td>
- 				<td>Empleada por días</td>
- 				<td>No se que pueda ser esto</td>
- 				<td>Observaciones para cliente de prueba</td>
-	 		</tr>
-	 	</tbody>
-	 </table>
-
-	 <?php echo site_url(); ?>
-</div>	
+    <h3><strong>CLIENTES</strong></h3>
+    <div id="toolbar" class="btn-group">
+        <button type="button" class="btn btn-default">
+            <img src="<?php echo base_url(); ?>\assets\image\add.jpg" class="img-rounded img-header"/>
+        </button>
+        <button type="button" class="btn btn-default">
+            <img src="<?php echo base_url(); ?>\assets\image\edit.jpg" class="img-rounded img-header"/>
+        </button>
+        <button type="button" class="btn btn-default">
+            <img src="<?php echo base_url(); ?>\assets\image\delete.jpg" class="img-rounded img-header"/>
+        </button>
+        <table class="table table-striped table-bordered"  data-search="true">
+            <thead>
+            <th>Documento</th>
+            <th>Nombre</th>
+            <th>Ciudad</th>
+            <th>Direcci&oacute;n</th>
+            <th>Telefono</th>
+            <th>Celular</th>
+            <th>Correo</th>
+            <th>Requerimiento</th>
+            <th>Harck Observaciones</th>
+            <th>Observaciones</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>123456</td>
+                    <td>Michael Alexander Espinosa Rico</td>
+                    <td>Bogotá</td>
+                    <td>Calle 118 # 20 - 53</td>
+                    <td>8621208</td>
+                    <td>3214325244</td>
+                    <td>maes9104@gmail.com</td>
+                    <td>Empleada por días</td>
+                    <td>No se que pueda ser esto</td>
+                    <td>Observaciones para cliente de prueba</td>
+                </tr>	
+                <tr>
+                    <td>123456</td>
+                    <td>Michael Alexander Espinosa Rico</td>
+                    <td>Bogotá</td>
+                    <td>Calle 118 # 20 - 53</td>
+                    <td>8621208</td>
+                    <td>3214325244</td>
+                    <td>maes9104@gmail.com</td>
+                    <td>Empleada por días</td>
+                    <td>No se que pueda ser esto</td>
+                    <td>Observaciones para cliente de prueba</td>
+                </tr>
+            </tbody>
+        </table>
+        //<?php echo site_url(); ?>
+    </div>	
 </div>
