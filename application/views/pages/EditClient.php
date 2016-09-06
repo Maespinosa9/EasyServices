ccd4132921e05d1ae3968f8a30635f1b9c34f636
Michael Espinosa <maespinosa9@misena.edu.co>
2016-09-06 01:24:40 -0500

    Form Editar Cliente

--- application/views/pages/EditClient.php
+++ application/views/pages/EditClient.php
@@ -1,1 +1,185 @@
-Estaremos Editando los clientes
+<div id="example" class="container" style="opacity: 1; visibility: visible;">
+    <section class="well">
+        <h2 class="ra-well-title">Edición de Cliente</h2>
+        <hr>
+        <form class="form-horizontal" role="form">
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="DOC_CLIENTE" class="control-label">Nro. Documento</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="DOC_CLIENTE" name="DOC_CLIENTE" placeholder="Identificación" >
+                </div>
+                <div class="col-sm-2">
+                    <label for="CIUDAD_ID" class="control-label">Ciudad</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-dropdown" id="CIUDAD_ID" name="CIUDAD_ID" >
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="NOMBRES" class="control-label">Nombres</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="NOMBRES" placeholder="Nombres Completos" name="NOMBRES">
+                </div>
+                <div class="col-sm-2">
+                    <label for="APELLIDOS" class="control-label">Apellidos</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="APELLIDOS" name="APELLIDOS" placeholder="Apellidos Completos" >
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="DIRECCION" class="control-label">Dirección</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="DIRECCION" placeholder="Dirección" name="DIRECCION">
+                </div>
+                <div class="col-sm-2">
+                    <label for="BARRIO" class="control-label">Barrio</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="BARRIO" name="BARRIO" placeholder="Barrio" >
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="TELEFONO" class="control-label">Télefono</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="TELEFONO" placeholder="Tel" name="TELEFONO">
+                </div>
+                <div class="col-sm-2">
+                    <label for="CELULAR" class="control-label">Celular</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="CELULAR" name="CELULAR" placeholder="Cel" >
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="TIPO_VIVIENDA" class="control-label">Tipo de Vivienda</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-dropdown" id="TIPO_VIVIENDA"  name="TIPO_VIVIENDA">
+                </div>
+                <div class="col-sm-2">
+                    <label for="CANT_ADULTOS" class="control-label">Nro. de Adultos</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="CANT_ADULTOS" name="CANT_ADULTOS" >
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="CANT_NIÑOS" class="control-label">Nro. de Niños</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="CANT_NIÑOS"  name="CANT_NIÑOS">
+                </div>
+                <div class="col-sm-2">
+                    <label for="CANT_TER_EDAD" class="control-label">Nro. de 3ra Edad</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="CANT_TER_EDAD" name="CANT_TER_EDAD" >
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="EDAD_NIÑOS" class="control-label">Edad de los Niños</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="EDAD_NIÑOS"  name="EDAD_NIÑOS" placeholder="Edades separadas por coma (,)">
+                </div>
+                <div class="col-sm-2">
+                    <label for="OFERTA_SALARIO" class="control-label">Oferta Salarial</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="OFERTA_SALARIO" name="OFERTA_SALARIO" >
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="DIAS_DESCANSO" class="control-label">Dias Descanso</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="DIAS_DESCANSO"  name="DIAS_DESCANSO" placeholder="Días separados por coma (,)">
+                </div>
+                <div class="col-sm-2">
+                    <label for="MASCOTAS" class="control-label">Cant. Mascotas</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="MASCOTAS" name="MASCOTAS" >
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="HORARIO" class="control-label">Horario</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="HORARIO"  name="HORARIO" placeholder="Horario Laboral">
+                </div>
+               <div class="col-sm-2">
+                    <label for="REQUERIMIENTO" class="control-label">Requerimiento</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-dropdown" id="REQUERIMIENTO"  name="REQUERIMIENTO">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="CORREO_PERSONAL" class="control-label">Correo Personal</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="email" class="form-control k-textbox" id="CORREO_PERSONAL" name="CORREO_PERSONAL" >
+                </div>
+               <div class="col-sm-2">
+                    <label for="CORREO_CORPORATIVO" class="control-label">Correo Corporativo</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control k-textbox" id="CORREO_CORPORATIVO"  name="CORREO_CORPORATIVO">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="EMPRESA_CONTRATANTE" class="control-label">Empresa Contratante</label>
+                </div>
+                <div class="col-sm-4">
+                    <input type="email" class="form-control k-textbox" id="EMPRESA_CONTRATANTE" name="EMPRESA_CONTRATANTE" >
+                </div>
+               <div class="col-sm-2">
+                    <label for="CORREO_CORPORATIVO" class="control-label">Observaciones Harck</label>
+                </div>
+                <div class="col-sm-4">
+                    <textarea class="form-control k-textbox" id="HARCK_OBSERVACIONES" name="HARCK_OBSERVACIONES" ></textarea>
+                </div>
+            </div>
+            
+            <div class="form-group">
+               <div class="col-sm-2">
+                    <label for="OBSERVACIONES" class="control-label">Observaciones</label>
+                </div>
+                <div class="col-sm-10">
+                    <textarea class="form-control k-textbox" id="OBSERVACIONES" name="OBSERVACIONES" ></textarea>
+                </div>
+            </div>
+             
+            <div class="buttons-wrap">
+                <button class="k-button k-state-default">Cancel</button>
+                <button class="k-button k-state-default">Update</button>
+            </div>
+        </form>
+
+    </section>
+</div>	
