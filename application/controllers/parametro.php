e60d5cc581e5b605bed9eac68a56b6b7820eed99
Michael Espinosa <maespinosa9@misena.edu.co>
2016-09-06 22:57:25 -0500

    Inicio parametros

---
+++ application/controllers/parametro.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Parametro extends CI_Controller {
+    function __construct() {
+        parent::__construct();
+        //$this->load->model('cliente_model');
+    }
+    
+    function index() {
+        $this->load->view('templates/Header');
+        $this->load->view('pages/Parametro');
+        $this->load->view('templates/Footer');
+    }
+}
---
+++ application/views/pages/Parametro.php
@@ -0,0 +1,12 @@
+<div id="EasyContainer" name="EasyContainer">
+     <h3><strong>PARAMETROS</strong></h3>
+      <div id="toolbar" class="btn-group">
+        <a class="btn"><img src="<?php echo base_url(); ?>\assets\image\add.jpg" class="img-rounded img-header"/></a>
+        <a class="btn"><img src="<?php echo base_url(); ?>\assets\image\edit.jpg" class="img-rounded img-header"/></a>
+        <a class="btn"><img src="<?php echo base_url(); ?>\assets\image\delete.jpg" class="img-rounded img-header"/></a>
+    </div>
+       <div id="prin">
+        <div id="table"></div>
+       </div>
+    
+</div>
