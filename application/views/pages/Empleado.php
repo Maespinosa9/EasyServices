83d279a20e62305049d4ec1d65c980b1cea7d838
J. Andres Boyaca <J. Andres Boyaca@LAPTOP-F78PISG9>
2016-09-06 23:27:47 -0500

    Creacion de clase general de alertas, ajuste de table cliente y actualizacion de header

---
+++ application/views/pages/Empleado.php
@@ -0,0 +1,54 @@
+<div id="EasyContainer" class="EasyContainer">
+    <h3><strong>EMPLEADOS</strong></h3>
+    <div class="prueba">
+    </div>
+    <div class="row">
+        <div class="col-sm-offset-1 col-sm-10">
+            <div id="prin">
+                <div id="toolbar" class="btn-group">
+                    <a href='<?php echo base_url(); ?>Empleado/EditEmpleado' class="btn">
+                        <img  src="<?php echo base_url(); ?>\assets\image\add.jpg" class="img-rounded img-header"  />
+                    </a>
+                    <a class="btn"><img src="<?php echo base_url(); ?>\assets\image\edit.jpg" class="img-rounded img-header"/></a>
+                    <a class="btn"><img src="<?php echo base_url(); ?>\assets\image\delete.jpg" class="img-rounded img-header"/></a>
+                </div>
+                <div id="table"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    $(document).ready(function () {
+        $("#table").kendoGrid({
+            dataSource: {
+                type: "JSON",
+                transport: {
+                    read: "<?= base_url() ?>data.json"
+                },
+                pageSize: 20
+            },
+            height: 550,
+            groupable: true,
+            sortable: true,
+            pageable: {
+                refresh: true,
+                pageSizes: true,
+                buttonCount: 5
+            },
+            columns: [{
+                    field: "id",
+                    title: "id",
+                    width: 240
+                }, {
+                    field: "name",
+                    title: "name"
+                }, {
+                    field: "price",
+                    title: "price"
+                }]
+        });
+    });
+</script>
+
+
+
